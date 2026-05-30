bbf974dcf22ff169b4cdcf9ee220d12c155a1d23
Andrii <andriiilitych218@gmail.com>
2026-05-30 20:28:41 +0300

    Fix Vercel, UTF-8, OKX validation, diagnostics

--- START_BOT.bat
+++ START_BOT.bat
@@ -1,5 +1,10 @@
 @echo off
+chcp 65001 >nul
 setlocal
+set LANG=uk_UA.UTF-8
+set LC_ALL=uk_UA.UTF-8
+set PYTHONIOENCODING=utf-8
+set npm_config_unicode=true
 cd /d "%~dp0"
 if not exist node_modules (
   call npm install
