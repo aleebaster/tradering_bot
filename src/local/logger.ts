9eb4c44074b874b4448831d80dcbb004a065f904
Andrii <andriiilitych218@gmail.com>
2026-05-31 21:34:42 +0300

    Fix Vercel serverless logging

--- src/local/logger.ts
+++ src/local/logger.ts
@@ -1,6 +1,8 @@
 import pino from "pino";
 
+const usePrettyLogs = !process.env.VERCEL && process.env.NODE_ENV !== "production";
+
 export const logger = pino({
   level: "info",
-  transport: { target: "pino-pretty", options: { colorize: true, translateTime: "SYS:standard" } }
+  ...(usePrettyLogs ? { transport: { target: "pino-pretty", options: { colorize: true, translateTime: "SYS:standard" } } } : {})
 });
